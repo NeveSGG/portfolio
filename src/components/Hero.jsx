aaf1af4fa748600dc1ebb5ce95aa45c66eefd541
NeveSGG <neverovsg@gmail.com>
2022-07-04 16:09:56 +0600

    Header and main page are done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero () {
                 <h1 className='name justText'>Neverov Stepan.</h1>
                 <h1 className='whatami justText'>I build web applications on React.</h1>
                 <div className='middletextwrapper'>
-                    <p className='description justText'>I`m a front-end engineer specializing in building modern and responsive web-apps. Currently, I`m focused on building accessible, human-centered product for engineers teams at IT-laboratory.</p>
+                    <p className='description justText'>I`m a front-end engineer specializing in building modern and responsive web-apps. Currently, I`m focused on building accessible, human-centered product for engineers` teams at IT-laboratory.</p>
                 </div>
                 <button className='btn code herobtn'>Check out my works!</button>
             </div>
